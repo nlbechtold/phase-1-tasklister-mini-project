8d470d023c465dfb8873f69f61a06d3df5d1be38
Nancy Bechtold <nancyleebechtold@gmail.com>
2024-06-06 14:06:02 -0600

    task lister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  const form = document.getElementById("create-task-form")
+    form.addEventListener("submit",(event) => {
+      event.preventDefault()
+      if(event.target["new-task-description"].value.trim())
+      {
+      let temp = document.createElement("li")
+      temp.textContent = event.target["new-task-description"].value
+      document.getElementById("list").append(temp)
+      form.reset()
+  
+      } 
+      else 
+          {
+          alert('invalid input you silly goose')}
+
+      form.reset()
+})});
+
+
+  
+   
+    
+    
+
+  //     const input = document.getElementById("create-task-form");
+  //     form.addEventListener("submit", function()=> {
+  //       alert('invalid input'); 
+  //       if(form.target["new-task-description"].value.trim()){
+  //         form.addEventListener('click', function() {
+  //           alert('invalid input'); 
+  //     }
+  //   )
+  // }
+   
+  
